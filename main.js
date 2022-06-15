063bd98bb643120b88a295543527cef699f1b3cf
Your Name <you@example.com>
2022-06-15 16:23:28 -0400

    Fresh Save for 6/15/22

---
+++ main.js
@@ -0,0 +1,50 @@
+//    $('li').on('click', function() {
+//        $(this).animate({
+//            opacity: 0.0, 
+//            paddingLeft:'+=80'
+//        }, 500, function() {
+//            $(this).remove();
+//        });
+//    });
+//    {);
+//     
+
+
+
+//function greet() {
+//    alert("Welcome");
+//    
+//}
+//var $ls = $('.heading');
+//
+//$ls.on('click', greet);
+//                    
+
+
+//let $ls = $('li')
+//    $ls.on('click', greet, false);
+//    
+
+//function updateToday() {
+//    
+//    let update = $('.det');
+//    let today = new Date();
+//    //let update = document.getElementById('det');
+//    let hourNow = today.getHours();
+//    
+//    if (hourNow >= (8 + 5)) {
+//       update.innerHTML = hourNow; 
+//    
+//    }
+//}
+//let update = $('.det');
+//update.on('load', updateToday, false);
+
+//var update = $('.det').value;
+//
+//var today = new Date();
+//
+//update.Text(today);
+
+//alert(today);
+//update.addEventListener('load', updateToday, false);
